fdd77a1b9e621dddcfd2188f60712c31f96d047b
oleg634996 <olegkolodiy22@gmail.com>
2022-01-31 16:13:02 +0200

    sadda

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Form from './form/form';
+import Contact from './contatcts/contacts';
 
 import './App.css';
 import { Component } from 'react/cjs/react.production.min';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
@@ -23,9 +29,24 @@ class App extends Component {
         }));
   };
 
+  onDeleteContact = data => {
+    console.log(data);
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.name !== data),
+    }));
+  };
+
   render() {
     console.log(this.state.contacts);
-    return <Form onSubmitForm={this.onSubmitForm} />;
+    return (
+      <div>
+        <Form onSubmitForm={this.onSubmitForm} />,
+        <Contact
+          contacts={this.state.contacts}
+          onDeleteContact={this.onDeleteContact}
+        />
+      </div>
+    );
   }
 }
 
