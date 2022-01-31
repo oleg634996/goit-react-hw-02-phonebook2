a6d763bd2466bdbfbe1eec2a2aaf1f3cd82ace05
oleg634996 <olegkolodiy22@gmail.com>
2022-01-31 20:33:40 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Form from './form/form';
 import Contact from './contatcts/contacts';
-import Search from './seargh/search';
+import Search from './search/search';
 
 import './App.css';
 import { Component } from 'react/cjs/react.production.min';
